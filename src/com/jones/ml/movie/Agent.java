f4ff0b07b8ea4563c1b2a074d885346892ef639c
callumjones17 <s3601120@student.rmit.edu.au>
2018-12-02 16:59:13 +1100

    Add Agent Class Functions

--- src/com/jones/ml/movie/Agent.java
+++ src/com/jones/ml/movie/Agent.java
@@ -4,9 +4,23 @@
 package com.jones.ml.movie;
 
 /**
- * @author Callum Jones
+ * @author Callum Jones, 2018
  *
  */
 public class Agent {
 
+	private int numWeightings = 0;
+	
+	public Agent(int numWeightings) {
+		this.numWeightings = numWeightings;
+	}
+
+	public int getNumWeightings() {
+		return numWeightings;
+	}
+
+	public void setNumWeightings(int numWeightings) {
+		this.numWeightings = numWeightings;
+	}
+	
 }

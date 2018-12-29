57a290442d28abb15032ac38ba16773d33438f74
Callum <Callum@DESKTOP-14KEOOK>
2018-12-30 10:41:36 +1100

    Started working on NeuralNet

---
+++ src/com/jones/ml/movie/network/NeuralNet.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.jones.ml.movie.network;
+
+import java.util.*;
+import com.jones.ml.movie.*;
+
+/**
+ * @author Callum Jones, 2018
+ *
+ */
+public class NeuralNet {
+
+	public float runThroughNetworkOnce(List<Agent> agents, List<Float>data, NetworkMap networkMap) {
+		
+		
+		return 0.0f;
+	}
+	
+}

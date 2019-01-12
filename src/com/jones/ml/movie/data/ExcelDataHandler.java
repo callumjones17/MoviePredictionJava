066947097c55f24da8c623d6e0c77763d63a2416
callumjones17 <s3601120@student.rmit.edu.au>
2019-01-12 11:39:04 +1100

    Fix Excel

--- src/com/jones/ml/movie/data/ExcelDataHandler.java
+++ src/com/jones/ml/movie/data/ExcelDataHandler.java
@@ -18,28 +18,8 @@ public class ExcelDataHandler {
 	
 	private String filename = "trainingData.xls";
 	
-	public class JExcelHelper {
-		 
-	    public Map<Integer, List<String>> readJExcel(String fileLocation) 
-	      throws IOException, BiffException {
-	  
-	        Map<Integer, List<String>> data = new HashMap<>();
-	 
-	        Workbook workbook = Workbook.getWorkbook(new File(fileLocation));
-	        Sheet sheet = workbook.getSheet(0);
-	        int rows = sheet.getRows();
-	        int columns = sheet.getColumns();
-	 
-	        for (int i = 0; i < rows; i++) {
-	            data.put(i, new ArrayList<String>());
-	            for (int j = 0; j < columns; j++) {
-	                data.get(i)
-	                  .add(sheet.getCell(j, i)
-	                  .getContents());
-	            }
-	        }
-	        return data;
-	    }
+	public ExcelDataHandler(String fileName) {
+		this.filename = fileName;
 	}
 	
 }

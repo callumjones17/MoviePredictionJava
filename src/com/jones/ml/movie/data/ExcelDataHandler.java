d93681f1045ddd684a10e14cccc818b798ea4c94
Callum <Callum@DESKTOP-14KEOOK>
2019-02-20 19:50:11 +1100

    Got early CSV file reading, and cell splitting to work.

--- src/com/jones/ml/movie/data/ExcelDataHandler.java
+++ src/com/jones/ml/movie/data/ExcelDataHandler.java
@@ -2,7 +2,7 @@
  * 
  */
 package com.jones.ml.movie.data;
-import java.io.File;
+import java.io.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -17,9 +17,48 @@ public class ExcelDataHandler {
 
 	
 	private String filename = "trainingData.xls";
+	public int test = 1;
 	
 	public ExcelDataHandler(String fileName) {
 		this.filename = fileName;
+	}
+	
+	public void readCSV() {
+		
+		String line = null;
+		List<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+		List<String> lineData = new ArrayList<String>();
+		
+		try {
+			
+			FileReader fileReader = new FileReader(filename);
+			
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while((line = bufferedReader.readLine()) != null) {
+				System.out.println(line);
+				
+				lineData = new ArrayList<String>();
+				for (String dataEntry: line.split(",")) {
+					lineData.add(dataEntry);
+				}
+				data.add((ArrayList<String>) lineData);
+				
+			}
+			
+			bufferedReader.close();
+			
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("File not Found!");
+			// Cast File Not Found Error Here
+		}
+		catch(IOException ex) {
+			// Cast Error for IO - Read Error / File in Use
+		}
+		
+		System.out.println(data.get(1).get(2));
+		
 	}
 	
 }
